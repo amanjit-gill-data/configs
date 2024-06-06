bc1cdccd97d5f909baadd477df96057b17293496
Amanjit Gill <amanjitgilldata@gmail.com>
2024-06-06 12:56:57 +1000

    remove test code from ipython config

--- ipython_config.py
+++ ipython_config.py
@@ -1,5 +1,6 @@
 from platform import python_version 
 
+# set startup banner e.g. "Python 3.10.12"
 c.InteractiveShell.banner1 = f"Python {python_version()}\n"
 
 # put newline between end of input and start of output
@@ -7,15 +8,4 @@ c.InteractiveShell.separate_out = "\n"
 
 # "Linux" scheme is for dark backgrounds
 c.InteractiveShell.colors = "Linux"
-
-# %% 
-
-from math import ceil 
-
-ceil(5.3)
-
-# %% 
-
-
-
 
