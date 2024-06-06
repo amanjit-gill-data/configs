5cc5fbbb8cf25f190d7d48805aab2431e9abed01
Amanjit Gill <amanjitgilldata@gmail.com>
2024-06-06 12:51:30 +1000

    rename confs; add ipython conf

---
+++ ipython_config.py
@@ -0,0 +1,21 @@
+from platform import python_version 
+
+c.InteractiveShell.banner1 = f"Python {python_version()}\n"
+
+# put newline between end of input and start of output
+c.InteractiveShell.separate_out = "\n"
+
+# "Linux" scheme is for dark backgrounds
+c.InteractiveShell.colors = "Linux"
+
+# %% 
+
+from math import ceil 
+
+ceil(5.3)
+
+# %% 
+
+
+
+
